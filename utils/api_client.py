fa69b7b07c266b96048ee43891aac3a18730a3a6
Brett Blomberg <bblomberg@live.com>
2025-12-16 10:04:19 -0800

    Update api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,13 +1,16 @@
 import requests
+from utils.config import BASE_URL, AUTH_TOKEN
+
 
 class APIClient:
-    def __init__(self, base_url, token=None):
-        self.base_url = base_url
+    def __init__(self):
+        self.base_url = BASE_URL
         self.headers = {
             "Content-Type": "application/json"
         }
-        if token:
-            self.headers["Authorization"] = f"Bearer {token}"
+
+        if AUTH_TOKEN:
+            self.headers["Authorization"] = f"Bearer {AUTH_TOKEN}"
 
     def get(self, endpoint):
         return requests.get(
