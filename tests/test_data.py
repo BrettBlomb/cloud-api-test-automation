c865d804b694974e1f947a95c20c834f8e119620
Brett Blomberg <bblomberg@live.com>
2025-12-16 09:53:16 -0800

    Create test_data.py

---
+++ tests/test_data.py
@@ -0,0 +1,13 @@
+from utils.api_client import APIClient
+
+def test_data_structure():
+    client = APIClient()
+    response = client.get("/posts/1")
+
+    body = response.json()
+
+    assert response.status_code == 200
+    assert "userId" in body
+    assert "id" in body
+    assert "title" in body
+    assert "body" in body
